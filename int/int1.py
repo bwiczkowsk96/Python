1b47d946e82e9a2d55ca3c45227fdd47eb934b54
bwiczkowski <33986657+vicebal@users.noreply.github.com>
2019-09-17 17:49:37 +0200

    update

--- int/int1.py
+++ int/int1.py
@@ -1,7 +1,13 @@
-
-
 name = 'Jan'
 age = 26
 daysInYear = 365
 message = '%s is %d years old, so is about %d days old'
 print(message % (name,age,age*daysInYear))
+
+
+name = 'Bartek'
+age = 23
+daysInYear = 365
+message = '{0:s} is {1:d} years old, so is about {2:d} days old'
+print(message.format(name,age,age*daysInYear))
+

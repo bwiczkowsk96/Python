6c6d56d5a7203389e1975f80f468eae2e71ea9ef
bwiczkowski <33986657+vicebal@users.noreply.github.com>
2019-09-17 17:56:06 +0200

    Update int1.py

--- int/int1.py
+++ int/int1.py
@@ -11,3 +11,11 @@ daysInYear = 365
 message = '{0:s} is {1:d} years old, so is about {2:d} days old'
 print(message.format(name,age,age*daysInYear))
 
+int2=1234567890
+int3=12345
+
+calk=int2//int3
+modul=int2%int3
+
+wynik= '{0:d} divided by {1:d} is  {2:d} and the rest is {3:d}'
+print(wynik.format(int2,int3,calk,modul))

7bd72f8f1abe0ebb997d6806ad6b82d604d94709
bwiczkowski <33986657+vicebal@users.noreply.github.com>
2019-09-18 18:54:36 +0200

    Update kolejnosc dzialan and logigal operations.py

--- logic operators/kolejnosc dzialan and logigal operations.py
+++ logic operators/kolejnosc dzialan and logigal operations.py
@@ -2,3 +2,9 @@ shoesize = 44
 number = (shoesize*5 +50)*20+1019 -1996
 print('shoe size is:',number //100)
 print('birth date is:',number %100)
+
+
+
+liczba=15
+numer = (liczba*2+10)/2-liczba
+print(numer)
